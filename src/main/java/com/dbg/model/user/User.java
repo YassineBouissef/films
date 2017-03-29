e7d5f0e189ce7652f4f3d9eae16d6108a6ab76ad
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-03-29 17:57:00 +0200

    jpa

--- src/main/java/com/dbg/model/user/User.java
+++ src/main/java/com/dbg/model/user/User.java
@@ -2,10 +2,19 @@ package com.dbg.model.user;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1443030971354997994L;
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 
 	private Integer name;
