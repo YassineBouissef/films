2e942f7a4a1f389274764620f9f538f04678f0b7
David Borrego Gutiérrez <dbgjerez@gmail.com>
2017-03-29 17:52:36 +0200

    created model

---
+++ src/main/java/com/dbg/model/evaluation/Evaluation.java
@@ -0,0 +1,25 @@
+package com.dbg.model.evaluation;
+
+public class Evaluation {
+
+	private Integer id;
+
+	private Integer points;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getPoints() {
+		return points;
+	}
+
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
+
+}
---
+++ src/main/java/com/dbg/model/film/Film.java
@@ -0,0 +1,35 @@
+package com.dbg.model.film;
+
+public class Film {
+
+	private Integer id;
+
+	private String title;
+
+	private Integer year;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+}
